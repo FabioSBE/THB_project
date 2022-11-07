567b7298aee775daea9b612c33de23e89416211d
FabioSBE <fabiofreirediniz@gmail.com>
2022-11-07 19:17:38 -0300

    Input a menu

--- history_bash.py
+++ history_bash.py
@@ -11,10 +11,11 @@ def cont_commands(lf):
             command_list[line[12:]] = command_list.get(line[12:], 0) + 1    
         else:
             command_list[line[7:]] = command_list.get(line[7:], 0) + 1
+    return dict(reversed(sorted(command_list.items(), key=lambda item: item[1])))
                 
 #Recebe os arquivos txt e um lista vazia como parâmetros e retorna uma lista
 #onde cada item corresponde a uma linha do arquivo e excluindo repetições entre os arquivos do input.  
-def segracacao(f1):
+def segrecacao(f1):
     listaf1 = []
     for line in f1:
         line = line.rstrip()
@@ -27,7 +28,7 @@ def segracacao(f1):
 #Usuário insere comando a ser pesquisado
 def busca_comandos(cl):
     lista = {}
-    comando = input("Digite um comando: ")
+    comando = input("\nType a command: ")
     for x,y in cl.items():
         if x.startswith(comando):
             lista[x] = lista.get(x,y)
@@ -45,25 +46,46 @@ def comad_principais(lista_final):
             command_princ[x[1]] = command_princ.get(x[1], 0) + 1
     
     return (dict(reversed(sorted(command_princ.items(), key=lambda item: item[1]))))
+
+def import_files():
+    finput = input("\nDigite o nome do arquivo contendo a lista geral: ")
+    try:
+        fhist01 = open(finput)
+        for a in fhist01:
+            a = a.rstrip()
+            segrecacao(open(a))
+        print ('\nLoading is Complete!!\n')
+    except:
+        print ("\nArquivo não encontrado!!!\n")
+    return finput
+
+def menu(l):
+    for a in open('menu.txt'):
+        a = a.rstrip()
+        print (a)
+    l = input ("\nSelect one option to go: ")
+    return l
     
 #Main
-print ("\n================================================================\n* Bem vindo ao resumo do history bash do seu terminal linux!!! *\n================================================================\n")
-finput = None
-while finput != 'n':
-    finput = input("\nDigite o nome do arquivo com os dados salvos do seu history\n(digite 'n' se quiser parar de ler arquivos): ")
-    if finput != 'n':
-        try:
-            fhist01 = open(finput)
-            segracacao(fhist01)
-        except:
-            print ("\nArquivo não encontrado!!!")
-                    
-cont_commands(lista_final)
-print (sum(command_list.values()))
-a = comad_principais(lista_final)
-print (a)
+loop = None
+while loop != '5':
+    loop = menu(loop)
+    if loop == '1':
+        import_files()
+        cont_commands(lista_final)
+    elif loop == '2':
+        print (cont_commands(lista_final))
+    elif loop == '3':
+        a = comad_principais(lista_final)
+        print (a)
+    elif loop == '4':
+        print (busca_comandos(command_list))
+    
+    
+        
 
-print (busca_comandos(command_list))
+
+
 
 
 #indices = int(comparar_juntar(fhist01, fhist02, fhist03))
