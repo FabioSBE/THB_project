a28592eac5bcde6364abf397ebf9a20acea28e61
FabioSBE <fabiofreirediniz@gmail.com>
2022-11-07 17:24:57 -0300

    User write the files names one by one

--- history_bash.py
+++ history_bash.py
@@ -1,8 +1,6 @@
-fhist01 = open ('ubuntu_history.txt')
-fhist02 = open ('h_bash.txt')
-fhist03 = open ('bash_h_ul.txt')
 command_list = {}
 lista_final = []
+a = {}
 
 #Recebe uma lista e insere os itens em dicionário['keys'] e conta suas ocorrências em
 #dicionário['value']
@@ -10,36 +8,21 @@ def cont_commands(lf):
     for line in lf:
         line = line.rstrip()
         if line[7:].startswith('sudo'):
-            command_list[line[11:]] = command_list.get(line[11:], 0) + 1    
+            command_list[line[12:]] = command_list.get(line[12:], 0) + 1    
         else:
             command_list[line[7:]] = command_list.get(line[7:], 0) + 1
                 
-#Receber os arquivos txt como parâmetros e a lista vazia e retorna uma lista
-#onde cada item corresponde a uma linha do arquivo e excluindo repetições.  
-def outra_maneira(f1,f2,f3,lf):
+#Recebe os arquivos txt e um lista vazia como parâmetros e retorna uma lista
+#onde cada item corresponde a uma linha do arquivo e excluindo repetições entre os arquivos do input.  
+def segracacao(f1):
     listaf1 = []
-    listaf2 = []
-    listaf3 = []
     for line in f1:
         line = line.rstrip()
         listaf1.append(line)
-    for line in f2:
-        line = line.rstrip()
-        listaf2.append(line)
-    for line in f3:
-        line = line.rstrip()
-        listaf3.append(line)
-
     for v in listaf1:
-        if v not in lf:
-            lf.append(v)
-    for v in listaf2:
-        if v not in lf:
-            lf.append(v)
-    for v in listaf3:
-        if v not in lf:
-            lf.append(v)
-    return (lf)
+        if v not in lista_final:
+            lista_final.append(v)
+    
 
 #Usuário insere comando a ser pesquisado
 def busca_comandos(cl):
@@ -49,15 +32,40 @@ def busca_comandos(cl):
         if x.startswith(comando):
             lista[x] = lista.get(x,y)
                         
-    print (dict(reversed(sorted(lista.items(), key=lambda item: item[1]))))
+    return dict(reversed(sorted(lista.items(), key=lambda item: item[1])))
+
+#Ranqueia os principais comandos utilizados pelo usuário
+def comad_principais(lista_final):
+    command_princ = {}
+    for x in lista_final:
+        x = x.rsplit()
+        if x[1] == 'sudo':
+            command_princ[x[2]] = command_princ.get(x[2], 0) + 1
+        else:
+            command_princ[x[1]] = command_princ.get(x[1], 0) + 1
     
-
-lista_final = outra_maneira(fhist01,fhist02,fhist03,lista_final)
+    return (dict(reversed(sorted(command_princ.items(), key=lambda item: item[1]))))
+    
+#Main
+print ("\n================================================================\n* Bem vindo ao resumo do history bash do seu terminal linux!!! *\n================================================================\n")
+finput = None
+while finput != 'n':
+    finput = input("\nDigite o nome do arquivo com os dados salvos do seu history\n(digite 'n' se quiser parar de ler arquivos): ")
+    if finput != 'n':
+        try:
+            fhist01 = open(finput)
+            segracacao(fhist01)
+        except:
+            print ("\nArquivo não encontrado!!!")
+                    
 cont_commands(lista_final)
-#print (command_list.items())
-#print (dict(sorted(command_list.items(), key=lambda item: item[1])))
+print (sum(command_list.values()))
+a = comad_principais(lista_final)
+print (a)
 
-busca_comandos(command_list)
+print (busca_comandos(command_list))
+
+
 #indices = int(comparar_juntar(fhist01, fhist02, fhist03))
 #print (type(indices))
 #uniao_arquivo(fhist01, indices)
