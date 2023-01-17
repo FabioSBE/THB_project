8b61756d572a3dc6f707dee8791d40e08877d930
FabioSBE <fabiofreirediniz@gmail.com>
2023-01-17 17:35:45 -0300

    Create files main.py e history_bash.py

--- history_bash.py
+++ history_bash.py
@@ -1,3 +1,7 @@
+import os
+
+path = "./inputs/" 
+
 c_dict_all = {}
 c_dict_most = {}
 c_dict_search = {}
@@ -12,17 +16,7 @@ def cont_com(lf):
         else:
             c_dict_all[line[7:]] = c_dict_all.get(line[7:], 0) + 1
     return dict(reversed(sorted(c_dict_all.items(), key=lambda item: item[1])))
-                
-#Takes the txt files and an empty list as parameters and returns a list where each item corresponds to a line in the file and excluding repetitions between the input files.
-def union_com(f1):
-    listf1 = []
-    for line in f1:
-        line = line.rstrip()
-        listf1.append(line)
-    for v in listf1:
-        if v not in final_list:
-            final_list.append(v)
-    
+      
 #User enters command to be searched
 def search_com(cl):
     lista = {}
@@ -34,33 +28,51 @@ def search_com(cl):
     return dict(reversed(sorted(lista.items(), key=lambda item: item[1])))
 
 #Rank the main commands used by the user
-def main_com(final_list):
+def main_com(f2):
     command_princ = {}
-    for x in final_list:
+    for x in f2:
         x = x.rsplit()
-        if x[1] == 'sudo':
-            command_princ[x[2]] = command_princ.get(x[2], 0) + 1
-        else:
-            command_princ[x[1]] = command_princ.get(x[1], 0) + 1
+        try:    
+            if x[1] == 'sudo':
+                command_princ[x[2]] = command_princ.get(x[2], 0) + 1
+            else:
+                command_princ[x[1]] = command_princ.get(x[1], 0) + 1
+        except:
+            pass
     
     return (dict(reversed(sorted(command_princ.items(), key=lambda item: item[1]))))
 
 #Import list of files in txt format
 def import_files():
-    #finput = input("\nInput the file path: ")
     try:
-        fhist01 = open('files_comp.txt')
-        for a in fhist01:
-            a = a.rstrip()
-            union_com(open(a))
-        print ('\n**** Loading is Complete!! **** \n')
+        for filename in os.listdir(path):
+            listf1 = []
+            if filename.endswith(".txt"):
+                with open(os.path.join(path, filename)) as file:
+                    # union_com(file)
+                    for line in file:
+                        line = line.rstrip()
+                        listf1.append(line)
+                    
+                    for v in listf1:
+                        if v not in final_list:
+                            final_list.append(v)
+        print ("\nLoad complete!!\n")
     except:
-        print ("\nFile not found!!!\n")
-    return
+        print ("\nFiles not found!\n")
+                
 
+    y = sorted (final_list)
+    ft = open("./commands/All.txt", 'w') 
+    for a in y:
+        ft.write(a)
+        ft.write('\n')
+        
+    return y
+                
 #Print options menu
 def menu(l):
-    for a in open('menu.txt'):
+    for a in open('./menus/menu.txt'):
         a = a.rstrip()
         print (a)
     l = input ("\nSelect one option to go: ")
@@ -68,40 +80,13 @@ def menu(l):
 
 #Save dictionaries to files
 def output_files (dicionario):
-    foutput = input("\nType a file name: ")
-    fteste = open(foutput, 'w')
+    path = "./outputs/"
+    fteste = open(os.path.join(path, input("\nType a file name: ")), 'w')
     for x,y in dicionario.items():
         fteste.write(x)
         fteste.write(',')
         fteste.write(str(y))
         fteste.write('\n')
 
-#Main
-loop = None
-while loop != '6':
-    loop = menu(loop)
-    if loop == '1':
-        import_files()
-        c_dict_all = cont_com(final_list)
-    elif loop == '2':
-        for x,y in c_dict_all.items():
-            print (x, ' - ', y)
-    elif loop == '3':
-        c_dict_most = main_com(final_list)
-        for x,y in c_dict_most.items():
-            print (x, ' - ', y)
-    elif loop == '4':
-        c_dict_search = search_com(c_dict_all)
-        for x,y in c_dict_search.items():
-            print (x, ' - ', y)
-    elif loop == '5':
-        for a in open('menu_save.txt'):
-            a = a.rstrip()
-            print (a)
-        c_dict_chose = input ("\nSelect one option to go: ")
-        if c_dict_chose == '1':
-            output_files(c_dict_all)
-        elif c_dict_chose == '2':
-            output_files(c_dict_most)
-        elif c_dict_chose == '3':
-            output_files(c_dict_search)
+
+    
---
+++ main.py
@@ -0,0 +1,33 @@
+import history_bash as a
+
+final_list = []
+
+loop = None
+while loop != '6':
+    loop = a.menu(loop)
+    if loop == '1':
+        final_list = a.import_files()
+        # print (final_list)
+        c_dict_all = a.cont_com(final_list)
+    elif loop == '2':
+        for x,y in c_dict_all.items():
+            print (x, ' - ', y)
+    elif loop == '3':
+        c_dict_most = a.main_com(final_list)
+        for x,y in c_dict_most.items():
+            print (x, ' - ', y)
+    elif loop == '4':
+        c_dict_search = a.search_com(c_dict_all)
+        for x,y in c_dict_search.items():
+            print (x, ' - ', y)
+    elif loop == '5':
+        for x in open('./menus/menu_save.txt'):
+            x = x.rstrip()
+            print (x)
+        c_dict_chose = input ("\nSelect one option to go: ")
+        if c_dict_chose == '1':
+            a.output_files(c_dict_all)
+        elif c_dict_chose == '2':
+            a.output_files(c_dict_most)
+        elif c_dict_chose == '3':
+            a.output_files(c_dict_search)
